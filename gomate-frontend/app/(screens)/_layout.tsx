360dc317d90db6e2bb918642409f58d1ffd5b3fe
tryAhmad <ahmadsaeed3220@gmail.com>
2025-08-20 13:37:29 +0500

    created home page

--- gomate-frontend/app/(screens)/_layout.tsx
+++ gomate-frontend/app/(screens)/_layout.tsx
@@ -4,6 +4,7 @@ const Layout = () => {
     return (
         <Stack>
             <Stack.Screen name="home" options={{ headerShown: false }} />
+            <Stack.Screen name="offers" options={{ headerShown: false }} />
             <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         </Stack>
     )
--- gomate-frontend/app/(screens)/home.tsx
+++ gomate-frontend/app/(screens)/home.tsx
@@ -4,16 +4,17 @@ import {
   View,
   ActivityIndicator,
   TouchableOpacity,
-  TextInput,
-  Button,
 } from "react-native";
 import MapView, { Marker, Region } from "react-native-maps";
 import * as Notifications from "expo-notifications";
 import * as Location from "expo-location";
-import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import BottomSheet, {
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
 import InputField from "@/components/InputField";
 import { icons } from "@/constants";
 import CustomButton from "@/components/CustomButton";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -28,23 +29,24 @@ const rideTypes = [
   {
     id: "bike",
     name: "Bike",
-    time: "5 min",
-    description: "Quick ride",
-    price: "200",
+    icon: <MaterialCommunityIcons name="motorbike" size={24} color="black" />,
   },
   {
     id: "car",
     name: "Car",
-    time: "7 min",
-    description: "Comfortable",
-    price: "600",
+    icon: <MaterialCommunityIcons name="car" size={24} color="black" />,
   },
   {
     id: "auto",
     name: "Auto",
-    time: "6 min",
-    description: "Economical",
-    price: "400",
+    icon: <MaterialCommunityIcons name="rickshaw" size={24} color="black" />,
+  },
+  {
+    id: "shared",
+    name: "Shared",
+    icon: (
+      <MaterialCommunityIcons name="account-multiple" size={24} color="black" />
+    ),
   },
 ];
 
@@ -52,7 +54,9 @@ const Home = () => {
   const [initialRegion, setInitialRegion] = useState<Region | null>(null);
   const mapRef = useRef<MapView | null>(null);
 
-  const [selectedRideType, setSelectedRideType] = useState<string | null>(null);
+  const [selectedRideType, setSelectedRideType] = useState<string | null>(
+    "bike"
+  );
   const [pickup, setPickup] = useState("");
   const [dropoff, setDropoff] = useState("");
   const [fare, setFare] = useState("");
@@ -87,6 +91,10 @@ const Home = () => {
   }, []);
 
   const handleFindRide = () => {
+    if (!selectedRideType || !pickup || !dropoff || !fare) {
+      alert("Please fill in all fields");
+      return;
+    }
     console.log({ selectedRideType, pickup, dropoff, fare });
     alert(
       `Finding ride for ${selectedRideType} from ${pickup} to ${dropoff} with fare ${fare}`
@@ -96,7 +104,9 @@ const Home = () => {
   return (
     <View className="flex-1 bg-white">
       <View className="px-4 mb-2 mt-[10%] bg-white z-10 shadow-md">
-        <Text className="text-3xl font-JakartaExtraBold text-center">Book Ride</Text>
+        <Text className="text-3xl font-JakartaExtraBold text-center">
+          Book Ride
+        </Text>
       </View>
       {initialRegion ? (
         <MapView
@@ -125,19 +135,21 @@ const Home = () => {
       <BottomSheet ref={sheetRef} index={0} snapPoints={snapPoints}>
         <BottomSheetView className="px-4">
           <Text className="text-2xl font-JakartaExtraBold mb-4">
-            Choose Ride Type
+            Choose Ride Type.
           </Text>
-          <View className="flex-row flex-wrap justify-around mb-4">
+          <View className="flex-row justify-around mb-4">
             {rideTypes.map((ride) => (
               <TouchableOpacity
                 key={ride.id}
                 onPress={() => setSelectedRideType(ride.id)}
-                className={`w-[50%] p-4 rounded-lg mb-3 border-2 ${
+                className={`w-[24%] p-4 rounded-lg mb-3 border-2 items-center ${
                   selectedRideType === ride.id
                     ? "border-blue-500 bg-blue-50"
                     : "border-gray-200 bg-gray-50"
                 }`}
               >
+                <Text className="text-sm text-gray-50">{ride.icon}</Text>
+
                 <Text
                   className={`font-JakartaBold text-xl ${
                     selectedRideType === ride.id
@@ -145,38 +157,31 @@ const Home = () => {
                       : "text-gray-800"
                   }`}
                 >
-                  {ride.name} ({ride.time})
-                </Text>
-                <Text className="text-base font-JakartaMedium text-gray-600">
-                  {ride.description}
-                </Text>
-                <Text
-                  className={`font-JakartaBold text-lg ${
-                    selectedRideType === ride.id
-                      ? "text-blue-600"
-                      : "text-gray-800"
-                  }`}
-                >
-                  {ride.price}
+                  {ride.name}
                 </Text>
               </TouchableOpacity>
             ))}
           </View>
+
           <InputField
             label="Pickup Location"
             placeholder="Enter Pickup Location"
             placeholderTextColor="grey"
             icon={icons.map}
-            value={pickup}
+            value={initialRegion?.latitude.toString()}
+            //value={pickup}
             onChangeText={setPickup}
+            //editable={false}
           />
           <InputField
             label="Drop-off Location"
             placeholder="Enter Destination"
             placeholderTextColor="grey"
             icon={icons.marker}
-            value={dropoff}
+            //value={dropoff}
+            value={initialRegion?.longitude.toString()}
             onChangeText={setDropoff}
+            //editable={false}
           />
           <InputField
             label="Fare Offer"
